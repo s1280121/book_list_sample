8c8d7c0743cd5b0cec7a9215b9e92d3d3b640fb3
s1280121 <s1280121@u-aizu.ac.jp>
2022-07-02 01:49:00 +0900

    Initial commit

---
+++ lib/book_list/book_list_page.dart
@@ -0,0 +1,68 @@
+import 'package:book_list_sample/book_list/book_list_model.dart';
+import 'package:book_list_sample/damain/book.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../add_book/add_book_page.dart';
+
+class BookListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<BookListModel>(
+      create: (_) => BookListModel()..fetchBookList(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('本一覧'),
+        ),
+        body: Center(
+          child: Consumer<BookListModel>(builder: (context, model, child) {
+            final List<Book>? books = model.books;
+
+            if (books == null) {
+              return CircularProgressIndicator();
+            }
+
+            final List<Widget> widgets = books
+                .map(
+                  (book) => ListTile(
+                    title: Text(book.title),
+                    subtitle: Text(book.author),
+              ),
+            ).toList();
+
+            return ListView(
+              children: widgets,
+            );
+          }),
+        ),
+        floatingActionButton:
+        Consumer<BookListModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            onPressed: () async {
+              // 画面遷移
+              final bool? added = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddBookPage(),
+                  fullscreenDialog: true,
+                ),
+              );
+
+              if (added != null && added) {
+                final snackBar = SnackBar(
+                  backgroundColor: Colors.green,
+                  content: Text('本を追加しました'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+
+              model.fetchBookList();
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          );
+        }),
+      ),
+    );
+  }
+}
